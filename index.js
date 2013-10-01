dcb4d884a736531fb6cbd5e449e72999eef2ab6d
Mike McNeil <michael.r.mcneil@gmail.com>
2013-10-01 16:06:09 -0500

    Boiled down all the generic stuff into waterline-blob

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+/**
+ * Module dependencies
+ */
+
+var fs			= require('fs'),
+	Glob		= require('glob').Glob,
+	_			= require('lodash'),
+	BlobFactory	= require('waterline-blob');
+
+
 /*---------------------------------------------------------------
 	:: Local Filesystem Adapter
 	:: sails-local-fs
@@ -16,67 +26,143 @@
 	which alleviates this limitation.
 
 ---------------------------------------------------------------*/
-
 module.exports = (function () {
+	
+	return BlobFactory({
 
-	var _ = require('lodash');
-	var GenericBlobAdapter = require('./GenericBlobAdapter');
-	var LocalDiskAdapter = require('./LocalDiskAdapter');
-	var LocalDisk = new GenericBlobAdapter (LocalDiskAdapter);
 
-	return {
+		/**
+		 * Read from provided uploadStream and write to blob store
+		 */
 
-		registerCollection: function (collection, cb) {
-			cb();
+		write: function  ( uploadStream, options, cb ) {
+
+			// Grab a logger (use sails.log if available)
+			var log = typeof sails !== 'undefined' ? sails.log : {
+				verbose: console.log,
+				warn: console.warn,
+				error: console.error
+			};
+
+			// When the uploadStream ends or errors, trigger the callback
+			uploadStream.once('end', function (err) {
+
+				if (err) {
+
+					// TODO:
+					// If the uploadStream is rejected, use this.files to lookup 
+					// and undo the writes, deleting the destination file(s) [configurable]
+					log.error('Error ::',err,':: occurred in upload stream...');
+					cb(err, uploadStream.files);
+					return;
+				}
+
+				if ( ! (uploadStream.files && _.keys(uploadStream.files).length ) ) {
+					log.verbose('No files specified! Closing stream...');
+				}
+				else log.verbose('Upload successful! Closing stream...');
+
+				cb(null, uploadStream.files);
+			});
+
+
+			// Listen to upload stream for new files
+			// Receive each upload as a paused field stream
+			uploadStream.on('data', function (pausedBinaryStream) {
+
+				// Build full path and open writestream for this file
+				var filename = pausedBinaryStream.filename;
+				var path = options.container + filename;
+				var destinationStream = fs.createWriteStream( path );
+
+				// Update uploadStream's reference to this file
+				// with adapter-specific information
+				// (namely the path)
+				var fileRecord = uploadStream.files[pausedBinaryStream._id];
+				fileRecord.path = path;
+
+				log.verbose('* ' + filename + ' :: Adapter received new file...');
+				
+				// Hook up the data events from the field streams 
+				// to the destination stream
+				pausedBinaryStream.pipe(destinationStream);
+				
+				// Resume field streams detected so far 
+				// and replay their buffers
+				log.verbose('* ' + filename + ' :: resuming stream...');
+				pausedBinaryStream._resume();
+			});
 		},
 
 
 
-
 		/**
-		 * `Adapter.write()`
-		 *
-		 * Pipe initial FieldStreams (files) into a destination stream,
-		 * then set up events to automatically pipe the FieldStream of any newly detected file
-		 * from the UploadStream to the destination stream
-		 *
-		 * @param {Stream} `uploadStream`	::	contains pausd field streams 
-		 *										and fires when new ones are added
-		 * @param {Object} `options`
-		 *			container		: {String} directory path where file(s) sould be stored
-		 *			maxBytes		: {Integer} Maximum combined size of all files together (default 1GB)
-		 *			maxBytesPerFile	: {Integer} Maximum file size for each individual file (default 25MB)
+		 * Read from blob store and write to specified download stream
 		 */
 
-		write: function (collectionName) {
-			// Slice off first argument (collectionName)
-			var args = Array.prototype.slice.call(arguments, 0);
-			args.shift();
+		read: function ( downloadStream, options, cb ) {
 
-			// Call underlying method
-			LocalDisk.write.apply(this, args);
-		},
+			// Grab a logger (use sails.log if available)
+			var log = typeof sails !== 'undefined' ? sails.log : {
+				verbose: console.log,
+				warn: console.warn,
+				error: console.error
+			};
 
+			// Makes callback optional, 
+			// and limits it to only fire once just in case
+			cb = cb ? _.once(cb) : function noopCb () {};
+			
+			var splat = options.container + '/' + options.filename;
+			log.verbose('LocalDiskAdapter.read files from ' + splat);
 
-		/**
-		 * Adapter.read()
-		 * Adapter.read(destinationStream)
-		 * Adapter.read(cb)
-		 * Adapter.read({})
-		 * Adapter.read({}, cb)
-		 * Adapter.read({}, destinationStream)
-		 */
+			// apply splat expression e.g. `.tmp/uploads/*` and return a set
+			var globtions = {};
 
-		read: function (collectionName) {
+			// Start glob stream
+			var glob = new Glob (splat, globtions);
 
-			// Slice off first argument (collectionName)
-			var args = Array.prototype.slice.call(arguments, 0);
-			args.shift();
+			// Handle end of the stream and errors
+			glob.once('abort', function globAborted () {
+				downloadStream.emit('glob_abort');
+				cb();
+			});
+			glob.once('end', function globDone (matches) {
+				downloadStream.emit('glob_done', matches);
+				cb(null, matches);
+			});
+			glob.once('error', function globError (err) {
+				err = err || new Error();
+				downloadStream.emit('glob_error', err);
+				cb(err);
+			});
+			
+			// Acquire source stream(s) one by one
+			// as files come in from glob
+			glob.on('match', function globMatch (path) {
+				log.verbose('Found file @', path);
 
-			// Call underlying method
-			LocalDisk.read.apply(this, args);	
+				// Pass byte stream to download stream
+				var pausedStream = fs.createReadStream(path, {
+					encoding: options.encoding
+				});
+
+				// Start buffering bytes that arrive
+				// pausedStream.pause();
+
+				// Figure out file name and save reference as `filename`
+				pausedStream.filename = pausedStream.path.match(/\/([^/]+)\/?$/)[1];
+
+				// Notify downloadStream that a new file has come in
+				downloadStream.emit('file', pausedStream);
+			});
+
+			// return destination stream
+			return downloadStream;
 		}
-	};
+	});
 
 })();
+
+
 
